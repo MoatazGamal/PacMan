f9fd44b6730907bc63b2c53fafe0a7f1b1400909
MoatazGamal <45331325+MoatazGamal@users.noreply.github.com>
2019-03-30 14:08:17 +0200

    Add files via upload

---
+++ PacMan/main.cpp
@@ -0,0 +1,70 @@
+#include<SFML/Graphics.hpp>
+#include"Player.h"
+#include "Animation.h"
+
+using namespace sf;
+
+
+
+
+
+int main() {
+
+	RenderWindow window(VideoMode(960, 960), "Pacman");
+
+	Player Pacman({ 100.0f,100.0f });
+
+	Texture texture;
+	texture.loadFromFile("map.png");
+	Sprite S_map(texture);
+	S_map.setScale(0.4, 0.4);
+	S_map.setPosition(100,50 );
+	
+	// start game loop.
+	while (window.isOpen())
+	{
+		Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+
+			if (Keyboard::isKeyPressed(Keyboard::Escape))
+				window.close();
+		}
+		//handel input;
+		Vector2f direction = { 0.0f,0.0f };
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+
+			direction.y -= 1.0f;
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Down))
+		{
+			direction.y += 1.0f;
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			direction.x += 1.0f;
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			direction.x -= 1.0f;
+		}
+		Pacman.SetDirection(direction);
+
+		//update model
+		Pacman.Update(1.0f / 60.0f);
+
+		window.clear();
+		window.draw(S_map);
+		Pacman.Draw(window);
+		
+		window.display();
+
+	}
+
+
+	return EXIT_SUCCESS;
+}
