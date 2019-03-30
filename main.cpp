bcd33b25328e6289cf801b4dbce982be07a5a806
MoatazGamal <45331325+MoatazGamal@users.noreply.github.com>
2019-03-30 14:48:50 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+//whenever you want to add something,change this main and do a pull-request.
+//if you have an extra class or header file, upload that header file using upload file.
+//NEVER upload folders, only headers,cpps and images or audio files.
+//we both well have the same main.cpp, copy and past the new files into your visual studio solution.
+
+//example
