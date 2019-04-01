491ed4043f2fff741ddf9926880d5fbaf2adb765
MoatazGamal <45331325+MoatazGamal@users.noreply.github.com>
2019-04-01 21:28:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,106 @@
 //NEVER upload folders, only headers,cpps and images or audio files.
 //we both well have the same main.cpp, copy and past the new files into your visual studio solution.
 
-//example
 
 #include<SFML/Graphics.hpp>
-#include"Player.h"
-#include "Animation.h"
+#include<iostream>
+
+#include "Tile.h"
 
 using namespace sf;
-
-
-
+//-------------------------------------------------------------------//
 
 
 int main() {
 
-	RenderWindow window(VideoMode(960, 960), "Pacman");
+	RenderWindow window(VideoMode(896, 992), "Pacman");
+	window.setFramerateLimit(100);
+	window.setKeyRepeatEnabled(false);
 
-	Player Pacman({ 100.0f,100.0f });
+	CircleShape pacman(20);
+	pacman.setFillColor(Color::Yellow);
+	pacman.setPosition(Vector2f(896 / 2, 992 / 2));
 
 	Texture texture;
-	texture.loadFromFile("map.png");
-	Sprite S_map(texture);
-	S_map.setScale(0.4, 0.4);
-	S_map.setPosition(100,50 );
+	texture.loadFromFile("resized_map.png");
+	Sprite s_map(texture);
+	Vector2f pac_start;
+	
+//-------------------------------------------------------------------//
+
+	Tile tiles[31][28]; //[y][x] y first then x
+	int tilesRepresent[31][28] =
+	{
+	  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	  {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	  {1, 8, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 8, 1},
+	  {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	  {1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	  {1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	  {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 6, 1, 1, 6, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 6, 1, 1, 6, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 6, 6, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 1, 1, 1, 1, 1, 0, 1, 1, 6, 1, 1, 1, 1, 1, 1, 1, 1, 6, 1, 1, 0, 1, 1, 1, 1, 1, 1},
+	  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	  {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1},
+	  {1, 8, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 8, 1},
+	  {1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
+	  {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
+	  {1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1},
+	  {1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+	  {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+	  {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+	  {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};
+	
+	for (int i = 0; i < 28; i++)
+	{
+		for (int j = 0; j < 31; j++)
+		{
+			tiles[j][i] = Tile(32*i+12,32*j+12);
+			switch (tilesRepresent[j][i])
+			{
+			case 1: // 1 is wall
+				tiles[j][i].wall = true;
+				break;
+			case 0: // 2 is a dot
+				tiles[j][i].dot = true;
+				break;
+			case 8: // 8 is a big_dot
+				tiles[j][i].big_dot = true;
+				break;
+			case 6: // 6 is blank space
+				tiles[j][i].eaten = true;
+				break;
+			case 5: // pacaman's starting position
+				pac_start = tiles[j][i].pos;
+			}
+		}
+	}
+	pacman.setPosition(pac_start.x -16,pac_start.y - 16);
+	/*Vector2f current_pos = pacman.getPosition();
+	int row = current_pos.x / 32 - 12;
+	int column = current_pos.y / 32 - 12;*/
+
+//-------------------------------------------------------------------//
 	
 	// start game loop.
 	while (window.isOpen())
 	{
-		Event event;
+//-------------------------------------------------------------------//
 
+		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
@@ -39,36 +110,63 @@ int main() {
 
 			if (Keyboard::isKeyPressed(Keyboard::Escape))
 				window.close();
-		}
-		//handel input;
-		Vector2f direction = { 0.0f,0.0f };
-		if (Keyboard::isKeyPressed(Keyboard::Up))
-		{
-
-			direction.y -= 1.0f;
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Down))
-		{
-			direction.y += 1.0f;
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Right))
-		{
-			direction.x += 1.0f;
-		}
-		else if (Keyboard::isKeyPressed(Keyboard::Left))
-		{
-			direction.x -= 1.0f;
-		}
-		Pacman.SetDirection(direction);
-
-		//update model
-		Pacman.Update(1.0f / 60.0f);
-
-		window.clear();
-		window.draw(S_map);
-		Pacman.Draw(window);
+			
 		
+		}
+//-------------------------------------------------------------------//
+		//handel input;
+		Vector2f current_pos = pacman.getPosition();
+		int row = current_pos.x / 32 - 12;
+		int column = current_pos.y / 32 - 12;
+		std::cout << pac_start.x<<" "<< pac_start.y<<std::endl;
+		
+	//	Vector2f direction;
+		if (event.key.code == Keyboard::Up)
+		{
+			if (tilesRepresent[row][column - 1] != 1)
+			{
+				pacman.move(0.0f, -1.0f);
+			}
+		}
+
+		else if (event.key.code == Keyboard::Down)
+		{
+			if (tilesRepresent[row][column + 1] != 1)
+			{
+				pacman.move(0.0f, 1.0f);
+			}
+		}
+		else if (event.key.code == Keyboard::Right)
+		{
+			if (tilesRepresent[row + 1][column] != 1)
+			{
+				pacman.move(1.0f, 0.0f);
+			}
+			
+		}
+		else if (event.key.code == Keyboard::Left)
+		{
+			if (tilesRepresent[row - 1][column] != 1)
+			{
+				pacman.move(-1.0f, 0.0f);
+			}
+		}
+//--------------------------------------------------------------------//
+		
+		window.clear();
+		window.draw(s_map);
+
+		for (int i = 0; i < 28; i++)
+		{
+			for (int j = 0; j < 31; j++)
+			{
+				tiles[j][i].show(window);
+			}
+		}
+		
+		window.draw(pacman);
 		window.display();
+//--------------------------------------------------------------------//end gameloop
 
 	}
 
